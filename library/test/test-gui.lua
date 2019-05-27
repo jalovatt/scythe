d380ce605a0d468386bb70c8116f5463fd5e53cc
Adam Lovatt <jalovatt@hotmail.com>
2019-05-26 21:00:22 -0600

    Add recent files listing and ExtStates to Test Runner

--- library/test/test-gui.lua
+++ library/test/test-gui.lua
@@ -18,22 +18,84 @@ if not libPath or libPath == "" then
 end
 loadfile(libPath .. "scythe.lua")()
 local GUI = require("gui.core")
+local T = require("public.table")[2]
 local test = require("test.core")
 
 local testFile = Scythe.scriptPath
+
+
+
+
+------------------------------------
+-------- Window settings -----------
+------------------------------------
+
+
+local window = GUI.createWindow({
+  name = "Test Runner",
+  w = 356,
+  h = 96,
+})
+
+
+
+------------------------------------
+-------- Logic ---------------------
+------------------------------------
+
+
+local recentFiles = T{}
+
+local function loadRecentFiles()
+  local fileStr = reaper.GetExtState("Scythe Test Runner", "recentFiles")
+  if not fileStr or fileStr == "" then return end
+
+  for file in fileStr:gmatch("[^|]+") do
+    recentFiles[#recentFiles + 1] = file
+  end
+end
+
+local function updateRecentFiles(file)
+  local _, existsIdx = recentFiles:find(function(v) return v == file end)
+  if existsIdx then
+    recentFiles:insert(1, recentFiles:remove(existsIdx))
+  else
+    recentFiles:insert(1, file)
+  end
+
+  if #recentFiles > 10 then recentFiles[11] = nil end
+
+  reaper.SetExtState("Scythe Test Runner", "recentFiles", recentFiles:concat("|"), true)
+end
 
 local function updateTestFile(file)
   testFile = file
   GUI.findElementByName("txt_file"):val(testFile)
 end
 
-local function selectFile()
-  local retval, userFile = reaper.GetUserFileNameForRead(testFile, "Choose a test file", ".lua")
-  if retval then
-    updateTestFile(userFile)
-  end
+
+local function showFilesMenu()
+  gfx.x = window.state.mouse.x
+  gfx.y = window.state.mouse.y
+
+  return gfx.showmenu("Browse...||" .. recentFiles:concat("|"))
 end
 
+local function selectFile()
+  local menu = showFilesMenu()
+
+  if menu == 1 then
+    local _, userFile = reaper.GetUserFileNameForRead(testFile, "Choose a test file", ".lua")
+    if userFile then
+      updateRecentFiles(userFile)
+      if userFile ~= testFile then updateTestFile(userFile) end
+      return
+    end
+  else
+    local file = recentFiles[menu - 1]
+    if file and file ~= testFile then updateTestFile(file) end
+  end
+end
 
 local testEnv = {
   describe = test.describe,
@@ -58,18 +120,6 @@ local function runTests()
 end
 
 
-------------------------------------
--------- Window settings -----------
-------------------------------------
-
-
-local window = GUI.createWindow({
-  name = "Test Runner",
-  w = 400,
-  h = 200,
-})
-
-
 
 
 ------------------------------------
@@ -83,7 +133,7 @@ layer:addElements( GUI.createElements(
   {
     name = "txt_file",
     type = "Textbox",
-    caption = "File:",
+    caption = "Test file:",
     x = 64,
     y = 8,
     w = 256,
@@ -93,15 +143,18 @@ layer:addElements( GUI.createElements(
     type = "Button",
     caption = "...",
     x = 324,
-    y = 8,
+    y = 10,
+    w = 24,
+    h = 20,
     func = selectFile,
   },
   {
     name = "btn_go",
     type = "Button",
     caption = "Run Tests",
-    x = 128,
-    y = 64,
+    x = 139,
+    y = 48,
+    w = 80,
     func = runTests
 
   }
@@ -111,5 +164,10 @@ window:addLayers(layer)
 
 GUI.Init()
 window:open()
+
+loadRecentFiles()
+if #recentFiles > 0 then
+  updateTestFile(recentFiles[1])
+end
 
 GUI.Main()
