53d95dc204dde01ab1db7943e77f8efde50e2b2e
Adam Lovatt <jalovatt@hotmail.com>
2019-11-02 22:29:02 -0600

    Adds initial tag parsing

--- development/doc-parser/Doc.lua
+++ development/doc-parser/Doc.lua
@@ -1,44 +1,92 @@
-local T = require("public.table")[2]
+local Table, T = require("public.table"):unpack()
 
-local Doc = {}
-
-Doc.Segment = {}
-Doc.Segment.__index = Doc.Segment
-function Doc.Segment:new()
-  local segment = {
-    content = {
-      name = nil,
-      description = T{},
-      param = T{},
-      option = T{},
-      ["return"] = T{},
-      signature = nil,
-    },
-    currentTag = "description",
-  }
-  return setmetatable(segment, self)
-end
-
-function Doc.Segment:push(line)
-  if not line:match("^%-%-") then
-    self.content.signature = line
-    self.content.name = self:nameFromSignature(line)
-  else
-    local tag, rest = line:match("@([^ ]+) (.+)")
-    if tag then self.currentTag = tag end
-
-    local content = tag and rest or line:match("^%-%-+ (.+)") or ''
-    self.content[self.currentTag]:insert(content)
-  end
-end
-
-function Doc.Segment:nameFromSignature(sig)
+local function nameFromSignature(sig)
   local key = sig:match("(.+) = function") or sig:match("function (.+) ?%(")
   local stripped = key:match("^local (.+)")
 
   return stripped or key
 end
 
+local parseTag = {
+  description = function(desc)
+    return desc:join("\n")
+  end,
+  param = function(params)
+    return params:reduce(function(acc, paramStr)
+      local param = {}
+      param.name, param.type, param.description = paramStr:match("([^ ]+) +([^ ]+) *(.*)")
+      acc:insert(param)
+      return acc
+    end, T{})
+  end,
+  option = function(option) end,
+  ["return"] = function(returns)
+    return returns:reduce(function(acc, returnStr)
+      local ret = {}
+      ret.type, ret.description = returnStr:match("([^ ]+) *(.*)")
+      acc:insert(ret)
+      return acc
+    end, T{})
+  end,
+  signature = function(signature) end,
+}
+
+local Segment = {}
+Segment.__index = Segment
+function Segment:new(line)
+  local segment = {
+    name = nil,
+    rawContent = T{
+      description = T{},
+      param = T{},
+      option = T{},
+      ["return"] = T{},
+      signature = nil,
+    },
+    parsedContent = T{},
+    currentTag = {
+      type = "description",
+      content = T{line},
+    },
+  }
+  return setmetatable(segment, self)
+end
+
+function Segment:closeTag()
+  self.rawContent[self.currentTag.type]:insert(self.currentTag.content:concat(" "))
+  self.currentTag = {
+    type = tag,
+    content = T{},
+  }
+end
+
+function Segment:push(line)
+  if line:match("^@") then
+    self:closeTag()
+    local tag, rest = line:match("@([^ ]+) (.+)")
+
+    self.currentTag = T{
+      type = tag,
+      content = T{rest}
+    }
+  else
+    self.currentTag.content:insert(line)
+  end
+end
+
+function Segment:finalize(line)
+  self:closeTag()
+  self.rawContent.signature = line
+  self.name = nameFromSignature(line)
+
+  self:process()
+end
+
+function Segment:process()
+  self.rawContent:forEach(function(v, k) self.parsedContent[k] = parseTag[k](v) end)
+end
+
+local Doc = {}
 function Doc.segmentsFromFile(filename)
   local segments = T{}
   local file = io.open(filename)
@@ -46,16 +94,15 @@ function Doc.segmentsFromFile(filename)
 
   for line in file:lines() do
     if line:match("^%-%-%-") then
-      currentSegment = Doc.Segment:new()
-      currentSegment:push(line)
+      currentSegment = Segment:new(line:match("^%-%-%- (.+)"))
     elseif currentSegment then
       if not line:match("^%-%-") then
-        currentSegment:push(line)
+        currentSegment:finalize(line)
         segments:insert(currentSegment)
 
         currentSegment = nil
       else
-        currentSegment:push(line)
+        currentSegment:push(line:match("^%-%-+ (.+)") or "")
       end
     end
   end
