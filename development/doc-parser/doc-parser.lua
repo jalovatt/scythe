77ce4ecb3f30b9343796b1a018be46b062537701
Adam Lovatt <jalovatt@hotmail.com>
2019-11-11 17:46:52 -0700

    Writes Markdown to a temp folder

--- development/doc-parser/doc-parser.lua
+++ development/doc-parser/doc-parser.lua
@@ -9,16 +9,41 @@ local Doc = require("doc-parser.Doc")
 local Md = require("doc-parser.Md")
 local Table, T = require("public.table"):unpack()
 
+local File = require("public.file")
+
+local libRoot = Scythe.libPath:match("(.*[/\\])".."[^/\\]+[/\\]")
+
 Scythe.wrapErrors(function()
-  local libRoot = Scythe.libPath:match("(.*[/\\])".."[^/\\]+[/\\]")
-  local file = libRoot .. "working/code-documentation/test-doc.lua"
-  local segments = Doc.segmentsFromFile(libPath .. "public/table.lua")
+  local pathOut = libRoot .. "temp/raw-docs"
+  if not (reaper.file_exists(pathOut) or reaper.RecursiveCreateDirectory(pathOut, 0) == 1) then
+    Msg("Unable to create " .. pathOut)
+    return
+  end
 
-  local mdSegments = segments:orderedReduce(function(acc, segment)
-    acc:insert(Md.parseSegment(segment.signature, segment.tags))
+  File.getFilesRecursive(libRoot, function(name, _, isFolder)
+    if isFolder and name:match("^%.git") then return false end
+    return isFolder or name:match("%.lua$")
+  end)
+    :forEach(function(file)
+      local doc = Doc.fromFile(file.path)
+      if not doc then return end
 
-    return acc
-  end, T{})
+      local strippedPath = file.path:match(libRoot .. "(.+)%.lua"):gsub("/", ".")
 
-  Msg(mdSegments:concat("\n"))
+      local md = doc:orderedMap(function(segment)
+        return Md.parseSegment(segment.name, segment.signature, segment.tags)
+      end):concat("\n")
+
+      local writePath = libRoot.."temp/raw-docs/"..strippedPath..".md"
+      local fileOut, err = io.open(writePath, "w+")
+      if not fileOut then
+        Msg("Error opening " .. writePath .. ": " .. err)
+        return
+      end
+
+      fileOut:write(md)
+      fileOut:close()
+
+      Msg("wrote: " .. writePath)
+    end)
 end)
